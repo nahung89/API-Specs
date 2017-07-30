75f8185ca61f36b2364de612a7def55c4ac7699a
Nah <nahung89@gmail.com>
2017-07-30 13:08:14 +0700

    update specs for Special

---
+++ Final_Special_API_endpoints.js
@@ -0,0 +1,472 @@
+// GET /v4.1/artists/{artist_id}
+{
+    "id": 606456507475634,
+    "name": "Justin Bieber",
+    "bio": "",
+    "description": "",
+    "square_image": {
+        "height": 400,
+        "mime_type": "image/jpeg",
+        "uri": "https://aimg.vibbidi-vid.com/vibbidi-images/artists/606456507475634.jpg",
+        "width": 400
+    },
+    "reaction": "dislike",
+    "cover_video": {
+        "id": 341478218630868,
+        "title": "Baby",
+        "creator": "Justin Bieber",
+        "cover_image": {
+            "uri": "https://berserker4.vibbidi-vid.com/vibbidi/videos/video___kffacxfA7G4.mp4.bgcover.jpg"
+        },
+        "uri": "https://berserker4.vibbidi-vid.com/vibbidi/videos/video___kffacxfA7G4.mp4"
+    },
+    "albums": [...],
+    "singles": [...],
+    "live_videos": [...],
+    "collections": [...],
+    "related_artists": [...],
+    "special_locked": false,
+    "special": [
+        {
+            "id": 26631701947,
+            "title": "Beauty and a Beat (feat. Nicki Minaj) [DJ Laszlo Body Rock Club Mix]",
+            "creator": "Justin Bieber",
+            "cover_image": {
+                "uri": "https://berserker3.vibbidi-vid.com/vibbidi/videos/video___vIl40DJzrXY___9jrpp2eyufd9rj8f2g3t4c7jg8.mp4.bgcover.jpg"
+            },
+            "uri": "http://qazwsx.vibbidi-vid.com/special/{start_point}__{end_point}__video___vIl40DJzrXY___9jrpp2eyufd9rj8f2g3t4c7jg8.mp4",
+            "duration": 450.8,
+            "mime_type": "video/mp4",
+            "width": 1280,
+            "height": 720,
+            "source_id": 717340540353713,
+            "size": 20181148,
+            "integrated_loudness": -7.4,
+            "player_audio_volume": 0.3715352290971725,
+            "type": "M",
+            "is_music_video": true,
+            "screen_shot_interval": 20,
+            "locked": false,
+            "is_collected": false
+        },
+        {
+            "id": 579828712513325,
+            "title": "Boyfriend (Joe Gauthreaux Dark Dub)",
+            "creator": "Justin Bieber",
+            "cover_image": {
+                "uri": "https://berserker5.vibbidi-vid.com/vibbidi/videos/video___X2DNUMyQcoY___5erxmxhprnrunp1xmhz8kn7du3.mp4.bgcover.jpg"
+            },
+            "uri": "http://qazwsx.vibbidi-vid.com/special/{start_point}__{end_point}__video___X2DNUMyQcoY___5erxmxhprnrunp1xmhz8kn7du3.mp4",
+            "duration": 466.33,
+            "mime_type": "video/mp4",
+            "width": 1280,
+            "height": 720,
+            "source_id": 717340540353713,
+            "size": 13815841,
+            "integrated_loudness": -8.2,
+            "player_audio_volume": 0.4073802778041127,
+            "type": "M",
+            "is_music_video": true,
+            "screen_shot_interval": 20,
+            "locked": false,
+            "is_collected": false
+        },
+        {
+            "id": 503432796866478,
+            "title": "Beauty and a Beat (DJ Laszlo Body Rock Instrumental)",
+            "creator": "Justin Bieber",
+            "cover_image": {
+                "uri": "https://berserker6.vibbidi-vid.com/vibbidi/videos/video___RSPRSsec2eI___nm5pdqu55tbxt38j7z1u5gu2zo.mp4.bgcover.jpg"
+            },
+            "uri": "http://qazwsx.vibbidi-vid.com/special/{start_point}__{end_point}__video___RSPRSsec2eI___nm5pdqu55tbxt38j7z1u5gu2zo.mp4",
+            "duration": 451.1,
+            "mime_type": "video/mp4",
+            "width": 480,
+            "height": 360,
+            "source_id": 717340540353713,
+            "size": 10430115,
+            "integrated_loudness": -7.5,
+            "player_audio_volume": 0.3758374042884441,
+            "type": "M",
+            "is_music_video": true,
+            "screen_shot_interval": 20,
+            "locked": false,
+            "is_collected": true
+        }
+    ]
+}
+//
+
+
+// GET /v4.1/artists/{artist_id}/special
+{
+    "scrolling": {
+        "start_point": 0,
+        "items": 3
+    },
+    "special": [
+        {
+            "id": 26631701947,
+            "title": "Beauty and a Beat (feat. Nicki Minaj) [DJ Laszlo Body Rock Club Mix]",
+            "creator": "Justin Bieber",
+            "artist_id": 606456507475634,
+            "artists": [
+                {
+                    "id": 606456507475634,
+                    "name": "Justin Bieber",
+                    "bio": "",
+                    "description": "",
+                    "square_image": {
+                        "height": 400,
+                        "mime_type": "image/jpeg",
+                        "uri": "https://aimg.vibbidi-vid.com/vibbidi-images/artists/606456507475634.jpg",
+                        "width": 400
+                    }
+                }
+            ],
+            "cover_image": {
+                "uri": "https://berserker3.vibbidi-vid.com/vibbidi/videos/video___vIl40DJzrXY___9jrpp2eyufd9rj8f2g3t4c7jg8.mp4.bgcover.jpg"
+            },
+            "uri": "http://qazwsx.vibbidi-vid.com/special/{start_point}__{end_point}__video___vIl40DJzrXY___9jrpp2eyufd9rj8f2g3t4c7jg8.mp4",
+            "duration": 450.8,
+            "mime_type": "video/mp4",
+            "width": 1280,
+            "height": 720,
+            "source_id": 717340540353713,
+            "size": 20181148,
+            "integrated_loudness": -7.4,
+            "player_audio_volume": 0.3715352290971725,
+            "type": "M",
+            "is_music_video": true,
+            "screen_shot_interval": 20,
+            "locked": false,
+            "is_collected": false
+        },
+        {
+            "id": 579828712513325,
+            "title": "Boyfriend (Joe Gauthreaux Dark Dub)",
+            "creator": "Justin Bieber",
+            "artist_id": 606456507475634,
+            "artists": [
+                {
+                    "id": 606456507475634,
+                    "name": "Justin Bieber",
+                    "bio": "",
+                    "description": "",
+                    "square_image": {
+                        "height": 400,
+                        "mime_type": "image/jpeg",
+                        "uri": "https://aimg.vibbidi-vid.com/vibbidi-images/artists/606456507475634.jpg",
+                        "width": 400
+                    }
+                }
+            ],
+            "cover_image": {
+                "uri": "https://berserker5.vibbidi-vid.com/vibbidi/videos/video___X2DNUMyQcoY___5erxmxhprnrunp1xmhz8kn7du3.mp4.bgcover.jpg"
+            },
+            "uri": "http://qazwsx.vibbidi-vid.com/special/{start_point}__{end_point}__video___X2DNUMyQcoY___5erxmxhprnrunp1xmhz8kn7du3.mp4",
+            "duration": 466.33,
+            "mime_type": "video/mp4",
+            "width": 1280,
+            "height": 720,
+            "source_id": 717340540353713,
+            "size": 13815841,
+            "integrated_loudness": -8.2,
+            "player_audio_volume": 0.4073802778041127,
+            "type": "M",
+            "is_music_video": true,
+            "screen_shot_interval": 20,
+            "locked": false,
+            "is_collected": false
+        },
+        {
+            "id": 503432796866478,
+            "title": "Beauty and a Beat (DJ Laszlo Body Rock Instrumental)",
+            "creator": "Justin Bieber",
+            "artist_id": 606456507475634,
+            "artists": [
+                {
+                    "id": 606456507475634,
+                    "name": "Justin Bieber",
+                    "bio": "",
+                    "description": "",
+                    "square_image": {
+                        "height": 400,
+                        "mime_type": "image/jpeg",
+                        "uri": "https://aimg.vibbidi-vid.com/vibbidi-images/artists/606456507475634.jpg",
+                        "width": 400
+                    }
+                }
+            ],
+            "cover_image": {
+                "uri": "https://berserker6.vibbidi-vid.com/vibbidi/videos/video___RSPRSsec2eI___nm5pdqu55tbxt38j7z1u5gu2zo.mp4.bgcover.jpg"
+            },
+            "uri": "http://qazwsx.vibbidi-vid.com/special/{start_point}__{end_point}__video___RSPRSsec2eI___nm5pdqu55tbxt38j7z1u5gu2zo.mp4",
+            "duration": 451.1,
+            "mime_type": "video/mp4",
+            "width": 480,
+            "height": 360,
+            "source_id": 717340540353713,
+            "size": 10430115,
+            "integrated_loudness": -7.5,
+            "player_audio_volume": 0.3758374042884441,
+            "type": "M",
+            "is_music_video": true,
+            "screen_shot_interval": 20,
+            "locked": false,
+            "is_collected": true
+        }
+    ]
+}
+
+Error = {
+    "error": {
+        "id": "api.forbidden.app_error",
+        "message": "The user is not allowed to perform the operation",
+        "detailed_error": "locked",
+        "vtrace_id": "usb94f6wf68ao5cpqpamzz4ffb",
+        "status_code": 403
+    }
+}
+//
+
+
+// POST /v4.1/artists/{artist_id}/unlockSpecial
+{
+    "success": true
+}
+//
+
+
+// GET /v4.1/users/{user_id}/collectedSpecial
+{
+    "scrolling": {
+        "start_point": 0,
+        "items": 2
+    },
+    "special": [
+        {
+            "id": 13641305775,
+            "title": "Missing Kissing",
+            "creator": "Eliza Doolittle",
+            "artist_id": 182947580393914,
+            "artists": [
+                {
+                    "id": 182947580393914,
+                    "name": "Eliza Doolittle",
+                    "bio": "",
+                    "description": "",
+                    "square_image": {
+                        "height": 400,
+                        "mime_type": "image/jpeg",
+                        "uri": "https://aimg.vibbidi-vid.com/vibbidi-images/artists/182947580393914.jpg",
+                        "width": 400
+                    }
+                }
+            ],
+            "cover_image": {
+                "uri": "https://berserker7.vibbidi-vid.com/vibbidi/videos/video___7EFMb60tD1c___4okwemtc7fgd4eg7bjac7ccgk3.mp4.bgcover.jpg"
+            },
+            "uri": "http://qazwsx.vibbidi-vid.com/special/{start_point}__{end_point}__video___7EFMb60tD1c___4okwemtc7fgd4eg7bjac7ccgk3.mp4",
+            "duration": 217.08,
+            "mime_type": "video/mp4",
+            "width": 1280,
+            "height": 720,
+            "source_id": 717340540353713,
+            "size": 9835273,
+            "integrated_loudness": -9.2,
+            "player_audio_volume": 0.457088189614875,
+            "type": "M",
+            "is_music_video": true,
+            "screen_shot_interval": 20,
+            "locked": true,
+            "is_collected": false
+        },
+        {
+            "id": 503432796866478,
+            "title": "Beauty and a Beat (DJ Laszlo Body Rock Instrumental)",
+            "creator": "Justin Bieber",
+            "artist_id": 606456507475634,
+            "artists": [
+                {
+                    "id": 606456507475634,
+                    "name": "Justin Bieber",
+                    "bio": "",
+                    "description": "",
+                    "square_image": {
+                        "height": 400,
+                        "mime_type": "image/jpeg",
+                        "uri": "https://aimg.vibbidi-vid.com/vibbidi-images/artists/606456507475634.jpg",
+                        "width": 400
+                    }
+                }
+            ],
+            "cover_image": {
+                "uri": "https://berserker6.vibbidi-vid.com/vibbidi/videos/video___RSPRSsec2eI___nm5pdqu55tbxt38j7z1u5gu2zo.mp4.bgcover.jpg"
+            },
+            "uri": "http://qazwsx.vibbidi-vid.com/special/{start_point}__{end_point}__video___RSPRSsec2eI___nm5pdqu55tbxt38j7z1u5gu2zo.mp4",
+            "duration": 451.1,
+            "mime_type": "video/mp4",
+            "width": 480,
+            "height": 360,
+            "source_id": 717340540353713,
+            "size": 10430115,
+            "integrated_loudness": -7.5,
+            "player_audio_volume": 0.3758374042884441,
+            "type": "M",
+            "is_music_video": true,
+            "screen_shot_interval": 20,
+            "locked": false,
+            "is_collected": true
+        }
+    ]
+}
+//
+
+
+// POST /v4.1/users/me/collectedSpecial
+// Form/JSON param: `id`: {special_id}, positive integer 64-bit
+// Form/JSON param: id=xxx
+{
+    "success": true
+}
+
+Error = {
+    "error": {
+        "id": "api.forbidden.app_error",
+        "message": "The user is not allowed to perform the operation",
+        "detailed_error": "special_id=13641305775",
+        "vtrace_id": "yojkbu3g5orpgfgjr4pke5tp1u",
+        "status_code": 403
+    }
+}
+//
+
+
+// DELETE /v4.1/users/me/collectedSpecial/{special_id}
+{
+    "success": true
+}
+//
+
+// GET /v4.1/special/{special_id}
+// Not Found!!!
+{
+    "id": 264594122172045,
+    "title": "Roll on the Red Tour 2004 at Hyde Park in England",
+    "cover_image": {
+        "uri": "https://berserker1.vibbidi-vid.com/vibbidi/videos/video___4u6bWs-ZG0o___csju58en5nnseewtocu8ansdbo.mp4.bgcover.jpg"
+    },
+    "uri": "https://berserker7.vibbidi-vid.com/vibbidi/videos/video___hLQl3WQQoQ0___1jpumz9w3entnbgahmhfeo85k7.mp4",
+    "duration": 7382.9,
+    "mime_type": "video/mp4",
+    "width": 1280,
+    "height": 720,
+    "size": 443823359,
+    "integrated_loudness": -11.1,
+    "player_audio_volume": 0.5688529308438415,
+    "screen_shot_interval": 20,
+    "artist_id": 499273824010207,
+    "creator": "Charli XCX",
+    "artists": [
+        {
+            "id": 499273824010207,
+            "name": "Charli XCX",
+            "bio": "",
+            "description": "",
+            "square_image": {
+                "height": 640,
+                "mime_type": "image/jpeg",
+                "uri": "https://i.scdn.co/image/532d7d184c3a8887932a29f4fb29fb4ff4665b40",
+                "width": 640
+            }
+        }
+    ],
+    "is_collected": true,
+    "tracks": [
+        {
+            "title": "A Head Full of Dreams",
+            "artist_id": 499273824010207,
+            "creator": "Charli XCX",
+            "artists": [
+                {
+                    "id": 499273824010207,
+                    "name": "Charli XCX",
+                    "bio": "",
+                    "description": "",
+                    "square_image": {
+                        "height": 640,
+                        "mime_type": "image/jpeg",
+                        "uri": "https://i.scdn.co/image/532d7d184c3a8887932a29f4fb29fb4ff4665b40",
+                        "width": 640
+                    }
+                }
+            ],
+            "start_point": 0.0,
+            "cover_image": {
+                "uri": "https://berserker1.vibbidi-vid.com/vibbidi/videos/video___4u6bWs-ZG0o___csju58en5nnseewtocu8ansdbo.mp4.bgcover.jpg"
+            }
+        },
+        {
+            "title": "Every Teardrop Is a Waterfall",
+            "artist_id": 499273824010207,
+            "creator": "Charli XCX",
+            "artists": [
+                {
+                    "id": 499273824010207,
+                    "name": "Charli XCX",
+                    "bio": "",
+                    "description": "",
+                    "square_image": {
+                        "height": 640,
+                        "mime_type": "image/jpeg",
+                        "uri": "https://i.scdn.co/image/532d7d184c3a8887932a29f4fb29fb4ff4665b40",
+                        "width": 640
+                    }
+                }
+            ],
+            "start_point": 378.2,
+            "cover_image": {
+                "uri": "https://berserker1.vibbidi-vid.com/vibbidi/videos/video___4u6bWs-ZG0o___csju58en5nnseewtocu8ansdbo.mp4.bgcover.jpg"
+            }
+        },
+        {
+            "title": "The Scientist",
+            "artist_id": 499273824010207,
+            "creator": "Charli XCX",
+            "artists": [
+                {
+                    "id": 499273824010207,
+                    "name": "Charli XCX",
+                    "bio": "",
+                    "description": "",
+                    "square_image": {
+                        "height": 640,
+                        "mime_type": "image/jpeg",
+                        "uri": "https://i.scdn.co/image/532d7d184c3a8887932a29f4fb29fb4ff4665b40",
+                        "width": 640
+                    }
+                }
+            ],
+            "start_point": 721.6,
+            "cover_image": {
+                "uri": "https://berserker1.vibbidi-vid.com/vibbidi/videos/video___4u6bWs-ZG0o___csju58en5nnseewtocu8ansdbo.mp4.bgcover.jpg"
+            }
+        }
+        /* ... */
+    ],
+    "n_tracks": 7
+}
+
+Error = {
+    "error": {
+        "id": "api.forbidden.app_error",
+        "message": "The user is not allowed to perform the operation",
+        "detailed_error": "special_id=805515888261252",
+        "vtrace_id": "e31ehtzuunnq9fu15z1y1k5xab",
+        "status_code": 403
+    }
+}
+
+
+
