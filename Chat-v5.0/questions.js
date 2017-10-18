6259ff791c9cf99860cd4d8b361fd9e1e5b80399
Nah <nahung89@gmail.com>
2017-10-18 09:32:53 +0700

    update answers

--- Chat-v5.0/questions.js
+++ Chat-v5.0/questions.js
@@ -15,30 +15,40 @@
 
 *People who are following each other*
 - People who are following each other, can chat any number of people per day
-    [!] Do you mean the number of message? Or you mean: If I have 10 followers, I can chat with all of them in a day. 
+    > If I have 10 followers, I can chat with all of them in a day.
 
 *People who are not following each other*
 - (MUST) People who are not following each other can only chat five people in 24 hours
-    [!] Does it mean I can start new conversation with only 5 max strangers, in 24 hours?
-    [!] And what if more than 5 strangers start new conversation with me? Do we allow this case?
+    > I can start new conversation with only 5 max strangers, in 24 hours.
+    > Allow the case more than 5 strangers start new conversation with me.
 
 - (MUST) Every time user invite a friend with his invitation code, the maximum number that can be chat with people who you are not following will increase by 10
-    [!] As I see in new pages, we are lacking of the function/page to identify the invitation code?
-    [!] Also, we might need to notify user that he/she gains the gift: "Congratulations, you can chat with xxx new friend now!"
+    > As I see in new pages, we are lacking of the function/page to identify the invitation code? -> in Sign Up
+    > Also, we might need to notify user that he/she gains the gift: "Congratulations, you can chat with xxx new friend now!" -> Yes
+
+- Initial: 5 people
+- 1 person after invitation: 15 people
+- 2 people after invitation: 25 people
+- 3 people after invitation: 35 people
+- 4 people after invitation: 45 people
+
+- (MUST) Approval is necessary to start a conversation with an unknown person
+
 
 *Need Facebook Connect*
 - (MUST) After pressing "Send" for the first time in chat
 - (MUST) After pressing "Send" for the first time in the comment input field
-    [!] If an user doesn't connect with FB account when send -> He/she can't send message?
+    > User need “FB connect” to chat.
 
+--------------------------
 
 - Page_1_p3
-    [!] Start a new conversation -> empty ?
-    [!] Already have conversation -> show content ?
+    > Start a new conversation -> empty
+    > Already have conversation -> show content
 
 - Page_1_p3 -> Page_2_p1
-    [!] How does chat dialog becomes full screen? By tap on chat input?
-    [!] If we tap back in Page_2_p1, the Page_1_p2 will re-appear?
+    > Chat dialog becomes full screen by tap on chat input? ->  The screen expands like the Fullscreen of Mac OS Finder.
+    > If we tap back in Page_2_p1, the Page_1_p2 will re-appear.
     [!] To open Page_2_p1 from Page_1_p3, I suggest that we have full-screen-button on top right menu to switch between fullsceen <-> dialog, is it okay?
 
 - Page_2, Page_3
@@ -51,7 +61,6 @@
     [!] Do I allow to send another message, or only one message to get the request?
     [!] I don't see the keyboard in 1a, 1b, 2a, 2b, does it mean I only send 1 message and have to wait for approval from Zebbie to send more?
     [!] The purple area will stay remaining, no matter Zebbie approves or rejects the request? And later even we chat a lot?
-    [!] The UI of purple area make me feel a dating app. Besides, I'm considering the case people just want to make friends (guy/guy, girl/girl..).
 
 - Page_7 (assume that Zebbie send me a request)
     [!] How Page_7_p1 appear when I'm in VTV page? And what happen if I have more than 1 invitation?
@@ -61,12 +70,15 @@
     [!] What is the 'Today' label on top mean?
     [!] Group continous messages could take times, pls consider the case that we seperate messages into individual rows (like https://zpl.io/bz84xkz).
 
-- Page_7, Page_8
-    [!] In case I have a lot of conversations, and I will have to switch between p2, p3a, p3b -> Everytime I get in a chat, it always appear a dialog and I have to enter full screen. From this point, it could be an uncomfortable UX.
-    [!] Is it okay if we put the the popup in p1 into a small view in p3a (under the messages)? It might be better when user gets a lot of requests.
-
 - Page_9
     [!] Does p1 only appear when I send 1st messsage on VIBBIDI, and won't show incase I reinstall app (with Fb connected before)?
     [!] And if I tap close in p1 (without connect Facebook) -> Stay in Page_2, Page_3, Page_4, Page_5 without sending message?
     [!] How does the invitation code work in our app?
+
+- Page_6
+    [!] The UI of purple area make me feel a dating app. Besides, I'm considering the case people just want to make friends (guy/guy, girl/girl..). -> Should we make it look more friendly?
+
+- Page_7, Page_8
+    [!] In case I have a lot of conversations, and I need to switch between p2, p3a, p3b -> Each time I get in a chat, it always appear a dialog and have to enter full screen. From this point, it could be an uncomfortable UX.
+    [!] Is it okay if we put the the popup in p1 into a small view in p3a (under the messages)? It might be better when user gets a lot of requests.```
 
